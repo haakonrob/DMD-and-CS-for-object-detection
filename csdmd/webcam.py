35918001b2464fdb7d14f384917fa9220168c231
haakonrob <haakon.robinson@gmail.com>
2020-10-16 12:29:58 +0200

    Refactored the background model

--- csdmd/webcam.py
+++ csdmd/webcam.py
@@ -9,100 +9,8 @@ from csdmd.viewer import cvViewer
 
 from csdmd.fps import FPS
 from csdmd.utils import isDigit
-
-from csdmd.streaming.Diff import Diff
-from csdmd.streaming.SDMD import SDMD
-from csdmd.streaming.STDMD import STDMD
-from csdmd.streaming.StreamingSnapshots import StreamingSnapshots, SlidingDMD
+from csdmd.bgmodel.BackgroundModel import BackgroundModel
 # from .OnlineDMD import OnlineDMD
-
-
-
-
-class BackgroundModel:
-    algs = [
-        {'class': StreamingSnapshots, 'type':'DMD', 'args': ['N', 'max_rank', 'dt']}, 
-        {'class': SlidingDMD, 'type':'DMD', 'args': ['N', 'max_rank', 'dt']}, 
-        {'class': Diff, 'type':'DMD', 'args': []}, 
-        {'class': cv.createBackgroundSubtractorMOG2, 'type':'CV', 'args': []}, 
-        {'class': cv.createBackgroundSubtractorKNN, 'type':'CV', 'args': []}, 
-    ]
-
-    def __init__(self):
-        self.model = None
-        self.params = None
-        self.bg = None
-        self.objects = None
-        self.reset(model=0, params=dict(max_rank=10, N=10, downsample=4, T=0, dt=1))
-
-    def reset(self, model=None, params=None):
-        if model is not None:
-            self.model_i = model
-        if params is not None:
-            self.params = params
-
-        if self.model_i < len(self.algs):
-            self.modeltype = self.algs[self.model_i]['type']
-            args = self.algs[self.model_i]['args']
-            self.model = self.algs[self.model_i]['class'](**{k:self.params[k] for k in args})
-        else:
-            print("Invalid algorithm")
-        
-        
-    def update(self, frame):
-        
-        self.frame_shape = frame.shape
-
-        if self.modeltype == 'DMD':
-            # Convert to grayscale and scale down
-            self._update_DMD(frame)
-        elif self.modeltype == 'CV':
-            self._update_CV(frame)
-    
-
-    def _update_DMD(self,frame): 
-        shape = self.get_downsampled_shape()     
-        y = cv.resize(frame, shape).T.flatten()
-        y = np.interp(y, (0, 255), (0,1))
-
-        ready = self.model.stream(y) 
-        if ready:
-            modes, _ = self.model.compute_modes()
-            if modes is not None:
-                bg = self.model.reconstruct(t=self.params['T']).real.clip(0,1).flatten()
-                objects = (y - bg).real.clip(0,1)
-
-                self.bg = bg.reshape(shape).T
-                self.objects = objects.reshape(shape).T
-
-                # bg = np.interp(bg,(0,1), (0,255))
-                # objects = np.interp(objects,(0,1), (0,255))
-        else:
-            self.bg = np.zeros(shape).T
-            self.objects = np.zeros(shape).T
-        
-            
-    def _update_CV(self, frame):
-        # Downsample to improve performance
-        shape = self.get_downsampled_shape()   
-        frame = cv.resize(frame, shape)
-
-        # Performance should be better with the raw image
-        fgmask = self.model.apply(frame)
-
-        # Median filter, this methods tend to yield a lot of noise
-        fgmask = cv.medianBlur(fgmask, 5)
-
-        self.bg = fgmask
-        self.objects = fgmask
-
-
-    def get_downsampled_shape(self):
-        return tuple( int(floor(x)/self.params['downsample']) for x in self.frame_shape[:2] )
-        
-
-
-
 
 
 def run(src=0):
@@ -126,7 +34,7 @@ def run(src=0):
             if will_quit:
                 break
             elif new_model is not None or new_params is not None:
-                model.reset(new_model, new_params)
+                model.reset(new_model=new_model, new_params=new_params)
             
             # Limit the FPS (also necessary for keeping track of the fps)
             toofast = fps.update()
@@ -140,15 +48,16 @@ def run(src=0):
             model.update(frame)
 
             # Info to show to overlay on the output
-            infostring = "FPS: {:.1f}, rank: {}, #snapshots: {}, Downsample: {}, Predict: {}".format(
+            infostring = "FPS: {:.1f}, rank: {}, #snapshots: {}, Downsample: {}, Model time: {:.2f}, Algorithm: {}".format(
                 fps.fps, 
                 model.params["max_rank"], 
                 model.params["N"], 
                 model.params['downsample'], 
-                "now" if model.params['T'] == 0 else "future"
+                model.params['T'],
+                model.name,
             )
             
-            viewer.show((frame, model.bg, model.objects), infostring)
+            viewer.show((frame, model.background, model.objects), infostring)
 
     finally:
         # When everything done, release the webcam or video file and close all windows
