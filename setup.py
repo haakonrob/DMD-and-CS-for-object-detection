1e6f06c605243d0ae0640f06d08ac52213818e2d
haakonrob <haakon.robinson@gmail.com>
2020-10-19 09:24:19 +0200

    Made __main__.py the entry point of the project.

--- setup.py
+++ setup.py
@@ -64,9 +64,9 @@ setup(
     #
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    # entry_points={  # Optional
-    #     'console_scripts': [
-    #         'sample=sample:main',
-    #     ],
-    # },
+    entry_points={  # Optional
+        'console_scripts': [
+            'sample=sample:main',
+        ],
+    },
 )
