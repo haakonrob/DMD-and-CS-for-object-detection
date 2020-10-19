971d9ee3f01bec3a4c6b4c80b8561adbf76b48a3
haakonrob <haakon.robinson@gmail.com>
2020-10-19 09:14:30 +0200

    Fixed the output of fps class

--- csdmd/fps.py
+++ csdmd/fps.py
@@ -2,13 +2,17 @@ from time import time
 
 
 class FPS:
-    def __init__(self, fps=60, alpha=0.8):
+    """
+        Keeps track of how many times per second the update() method is called.
+        Can be used as a rate-limiter by specifying the max_fps.
+    """
+    def __init__(self, max_fps=60, alpha=0.8):
         self.last = 0
-        self.fps = fps
-        self.interval = 1/fps
+        self.fps = 0
+        self.interval = 1/max_fps if max_fps is not None else 0
         self.alpha = alpha  # smoothing parameter
     
-    def update(self):
+    def update(self):xxxxxxx    
         t = time()
         elapsed = t - self.last
         if elapsed > self.interval:
